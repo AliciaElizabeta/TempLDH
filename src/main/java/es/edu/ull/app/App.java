3608049c2db3ee425b4a00e3839030e924dc34ff
alu0101221960 <72342937+alu0101221960@users.noreply.github.com>
2021-11-25 11:29:07 +0000

    Add files via upload
    Cambio System.out por logger.out

--- src/main/java/es/edu/ull/app/App.java
+++ src/main/java/es/edu/ull/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("Changed World");
+        logger.log("Hello World");
+        logger.log("Changed World");
     }
 }
